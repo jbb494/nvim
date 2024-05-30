a366fad4c4e53ec8cf08cb9c6c62c2f139625984
Joan Bellavista <jbb494@gmail.com>
2024-05-30 23:28:35 +0200

    Fixing grep on visual mode

--- lua/user/utils.lua
+++ lua/user/utils.lua
@@ -1,9 +1,33 @@
 M = {}
 
+local function getPositionsSorted(positions)
+    table.sort(positions, function(a, b)
+        if (a[2] < b[2]) then
+            return true
+        elseif (a[2] == b[2]) then
+            if (a[3] < b[3]) then
+                return true
+            else
+                return false
+            end
+        else
+            return false
+        end
+    end)
+end
+
 -- We have a problem here, that when the cursor is on the left it doesnt work.
 M.get_visual_selection = function()
-    local s_start = vim.fn.getpos("v")
-    local s_end = vim.fn.getpos(".")
+    local raw_start = vim.fn.getpos("v")
+    local raw_end = vim.fn.getpos(".")
+    local positions = { raw_start, raw_end }
+    vim.print(positions)
+    getPositionsSorted(positions)
+    vim.print(positions)
+
+    local s_start = positions[1]
+    local s_end = positions[2]
+
     local n_lines = math.abs(s_end[2] - s_start[2]) + 1
     local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
     lines[1] = string.sub(lines[1], s_start[3], -1)
